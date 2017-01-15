960ee3cc3412018fa1f7da83bba529bfa2537b4d
lucas-noleto <lucas_noleto@outlook.com>
2017-01-15 13:07:12 -0300

    Controllers dos membros /edit/delete/new/update/show

---
+++ app/controllers/membros_controller.rb
@@ -0,0 +1,53 @@
+class MembrosController < ApplicationController
+	
+	def index
+  		@membros = Membro.order :name
+	end
+
+	def new
+  		@membro = Membro.new
+	end
+
+	def show
+  		@membro = Membro.find(params[:id])
+	end
+
+	def destroy
+  		@membro = Membro.find(params[:id])
+  		@membro.destroy
+
+	  redirect_to(action: "index")
+	end
+
+	def membro_params
+ 	 params.require(:membro).permit(:name, :role, :email)
+	end
+
+	def create
+	  @membro = Membro.new(membro_params)
+	  if @membro.save
+	    redirect_to action: "show", id: @membro
+	  else
+	    render action: "new"
+	  end
+	end
+
+
+	def edit
+	  @membro = Membro.find params[:id]
+	end
+
+	def update
+	  @membro = Membro.find(params[:id])
+
+	  if @membro.update_attributes(membro_params)
+	    redirect_to action: "show", id: @membro
+	  else
+	    render action: "edit"
+	  end
+	end
+
+
+
+end
+
