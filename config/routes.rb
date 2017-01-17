a3c0e5b8b6d911754a4784f54d862e89afa93f68
Eduardo Bezerra <eduardocarioca55@hotmail.com>
2017-01-17 16:55:24 -0300

    Login_Admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :admins
   resources :advertencias
   devise_for :views
   devise_for :users
 
   resources :membros
+
+  root to: "membros#index"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
