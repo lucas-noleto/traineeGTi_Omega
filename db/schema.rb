6e88d7ea48079586b9959e80d718b212df04710d
adolfoggc <adolfo.ggc@gmail.com>
2017-01-23 01:08:59 -0300

    atualizando schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,66 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
+ActiveRecord::Schema.define(version: 20170117184256) do
+=======
 ActiveRecord::Schema.define(version: 20170117184538) do
+>>>>>>> pag_user
+
+  create_table "admins", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+<<<<<<< HEAD
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+=======
+    t.string   "cargo"
+    t.integer  "pontuacao"
+    t.string   "fb"
+    t.string   "trello"
+    t.string   "github"
+    t.integer  "advertencias"
+    t.string   "foto"
+    t.string   "nome"
+    t.date     "aniversario"
+    t.integer  "exp"
+    t.integer  "nivel"
+>>>>>>> pag_user
+  end
+
+  create_table "advertencias", force: :cascade do |t|
+    t.date     "advdata"
+    t.string   "reason"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "membro_id"
+  end
+
+  create_table "membros", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "course"
+    t.string   "role"
+    t.date     "JoinTime"
+    t.string   "Rg"
+    t.string   "CPF"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -26,21 +84,9 @@ ActiveRecord::Schema.define(version: 20170117184538) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "cargo"
-    t.integer  "pontuacao"
-    t.string   "fb"
-    t.string   "trello"
-    t.string   "github"
-    t.integer  "advertencias"
-    t.string   "foto"
-    t.string   "nome"
-    t.date     "aniversario"
-    t.integer  "exp"
-    t.integer  "nivel"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "views", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -55,9 +101,8 @@ ActiveRecord::Schema.define(version: 20170117184538) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_views_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_views_on_reset_password_token", unique: true
   end
-
-  add_index "views", ["email"], name: "index_views_on_email", unique: true
-  add_index "views", ["reset_password_token"], name: "index_views_on_reset_password_token", unique: true
 
 end
